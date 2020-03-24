e5631247b8e8f85d78a645197f40ff1923a96f0f
uzoma253 <uzomauzoegwu@gmail.com>
2020-03-24 14:03:29 +0100

    add files

---
+++ script.js
@@ -0,0 +1,13 @@
+var name = "Uzoegwu Uzoma Agnes";
+var courses = ["HTML", "CSS", "JavaScript"];
+console.log(name, courses)
+
+
+var odd_number;
+
+for (odd_number =0; odd_number <=200; odd_number++)
+{
+	if (odd_number%2 != 0) {
+		console.log(odd_number)
+	}
+}
